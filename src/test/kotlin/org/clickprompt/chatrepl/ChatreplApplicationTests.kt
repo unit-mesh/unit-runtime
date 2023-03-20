23ce905e9b6efba88920fab90360baf475a01192
Phodal Huang <h@phodal.com>
2023-03-20 11:51:49 +0800

    fix: fix build

--- src/test/kotlin/org/clickprompt/chatrepl/ChatreplApplicationTests.kt
+++ src/test/kotlin/org/clickprompt/chatrepl/ChatreplApplicationTests.kt
@@ -3,7 +3,7 @@ package org.clickprompt.chatrepl
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ChatreplApplicationTests {
 
 	@Test
