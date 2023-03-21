ce80d144607bba3ab598ed6fec508121a73504de
Phodal Huang <h@phodal.com>
2023-03-21 09:14:08 +0800

    feat: add framework into itnerpreter request

--- src/main/kotlin/org/clickprompt/flowrepl/repl/api/InterpreterRequest.kt
+++ src/main/kotlin/org/clickprompt/flowrepl/repl/api/InterpreterRequest.kt
@@ -7,5 +7,20 @@ data class InterpreterRequest(
     var id: Int = -1,
     val code: String,
     val language: String = "kotlin",
+    val framework: String = SupportedFramewrok.SPRING.toString(),
     val history: Boolean = false
 )
+
+enum class SupportedFramewrok {
+    SPRING, KTOR;
+
+    companion object {
+        fun fromString(value: String): SupportedFramewrok {
+            return when (value.lowercase()) {
+                "spring" -> SPRING
+                "ktor" -> KTOR
+                else -> throw IllegalArgumentException("Unknown framework: $value")
+            }
+        }
+    }
+}
