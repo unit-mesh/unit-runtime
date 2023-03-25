b34ca919846482e89465c276065bf0fa840522e3
Phodal Huang <h@phodal.com>
2023-03-25 18:19:42 +0800

    feat: make codemirror works

--- react-repl/src/app/page.tsx
+++ react-repl/src/app/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import styles from "./page.module.css";
+import { useState } from "react";
+import { CodeEditor } from "@/app/components/CodeEditor";
 
 export default function Home() {
+  const [code, setCode] = useState("console.log('hello, world')");
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-
+        <CodeEditor
+          value={code}
+          extensions={[]}
+          onChange={(newCode) => {
+            setCode(newCode);
+          }}
+        />
       </div>
     </main>
   );
