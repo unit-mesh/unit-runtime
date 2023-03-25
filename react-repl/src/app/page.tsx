62669a94cc9d67f5e622bef42cc9703b23fd3c56
Phodal Huang <h@phodal.com>
2023-03-25 18:36:42 +0800

    refactor: import babel deps #1

--- react-repl/src/app/page.tsx
+++ react-repl/src/app/page.tsx
@@ -1,11 +1,21 @@
 "use client";
 
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CodeEditor } from "@/app/components/CodeEditor";
 
 export default function Home() {
-  const [code, setCode] = useState("console.log('hello, world')");
+  const [code, setCode] =
+    useState(`const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello, world!</h1>);`);
+  const [compiled, setCompiled] = useState("");
+
+  useEffect(() => {
+    // transform(code, {
+    //   presets: ["@babel/preset-env", "@babel/preset-react"],
+    // });
+    // console.log(code);
+  }, [code]);
 
   return (
     <main className={styles.main}>
@@ -15,6 +25,14 @@ export default function Home() {
           extensions={[]}
           onChange={(newCode) => {
             setCode(newCode);
+          }}
+        />
+
+        <CodeEditor
+          value={compiled}
+          extensions={[]}
+          onChange={(compiledCode) => {
+            setCompiled(compiledCode);
           }}
         />
       </div>
