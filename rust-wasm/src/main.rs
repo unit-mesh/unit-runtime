474135334da5e92114857bf192c2168d4b79636b
CGQAQ <m.jason.liu@outlook.com>
2023-03-29 13:48:51 +0800

    feat: rust-wasm init

---
+++ rust-wasm/src/main.rs
@@ -0,0 +1,24 @@
+use wasmer::{ Store, Module, imports, Instance, Value };
+
+fn main() -> anyhow::Result<()> {
+    let module_wat = r#"
+    (module
+      (type $t0 (func (param i32) (result i32)))
+      (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+        get_local $p0
+        i32.const 1
+        i32.add))
+    "#;
+
+    let mut store = Store::default();
+    let module = Module::new(&store, &module_wat)?;
+    // The module doesn't import anything, so we create an empty import object.
+    let import_object = imports! {};
+    let instance = Instance::new(&mut store, &module, &import_object)?;
+
+    let add_one = instance.exports.get_function("add_one")?;
+    let result = add_one.call(&mut store, &[Value::I32(42)])?;
+    assert_eq!(result[0], Value::I32(43));
+
+    Ok(())
+}
