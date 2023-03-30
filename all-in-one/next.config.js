a3874bf184e540323c0bccbcb301360eb4c3aa87
CGQAQ <m.jason.liu@outlook.com>
2023-03-30 11:44:32 +0800

    feat: serviceworker regist route

--- all-in-one/next.config.js
+++ all-in-one/next.config.js
@@ -1,4 +1,4 @@
-const { InjectManifest } = require("workbox-webpack-plugin");
+const { InjectManifest, GenerateSW } = require("workbox-webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -9,13 +9,18 @@ const nextConfig = {
   // https://github.com/vercel/next.js/issues/33863#issuecomment-1140518693
   webpack: (config, { isServer }) => {
     if (!isServer) {
-      config.resolve.fallback.fs = false
-
       config.plugins.push(
         new InjectManifest({
           swSrc: "./sw.ts",
           swDest: "../public/sw.js",
           include: ["__nothing__"],
+        })
+      );
+
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          HOST: "http://localhost:9000",
+          BASE_URL: "/hmr"
         })
       );
     }
--- all-in-one/sw.ts
+++ all-in-one/sw.ts
@@ -1,5 +1,8 @@
 /// <reference lib="webworker" />
 
+import routing from "workbox-routing"
+import type {RouteHandlerCallbackOptions} from "workbox-core"
+
 /**
  * This is a workaround for the issue with workbox and next.js
  * https://github.com/vercel/next.js/issues/33863#issuecomment-1140518693
@@ -8,3 +11,35 @@
 const resources = (self as any).__WB_MANIFEST; // this is just to satisfy workbox
 
 // ... your SW code
+
+routing.registerRoute(
+    /^https?:\/\/HOST\/BASE_URL\/(\/.*)$/,
+    async ({
+      request,
+      params,
+      url,
+    }: RouteHandlerCallbackOptions): Promise<Response> => {
+      const req = request?.url || url.toString();
+      const [pathname] = params as string[];
+      // send the request to vite worker
+      const response = await postToViteWorker(pathname)
+      return response;
+    }
+  );
+
+
+  async function postToViteWorker(pathname: string): Promise<Response> {
+    self.postMessage({ pathname });
+
+    return new Promise((resolve) => {
+        self.addEventListener(
+            "message",
+            (event) => {
+            if (event.data.pathname === pathname) {
+                resolve(event.data.response as Response);
+            }
+            },
+            { once: true }
+        );
+    });
+  }
